6e7b30051cf3fd6441fa115fbffbcfbb1ac0f812
ST68122420115-ux <std.68122420115@ubru.ac.th>
2026-03-20 12:28:22 +0700

    Added first time

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your-secret-key"
-    SQLALCHEMY_DATABASE_URI = "postgresql://genshin_web_app_user:eDHECRP4cidXh2U5OkM1QJiRLLJyONdE@dpg-d6ucr9ngi27c73e650v0-a.virginia-postgres.render.com/genshin_web_app"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://genshin_web_app_user:eDHECRP4cidXh2U5OkM1QJiRLLJyONdE@dpg-d6ucr9ngi27c73e650v0-a.virginia-postgres.render.com/genshin_web_app"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
